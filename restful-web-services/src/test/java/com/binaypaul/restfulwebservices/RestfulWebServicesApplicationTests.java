d51bfd9d1e61789da90a0784e85817f2f8b79305
Binay Paul <binay.paul.1993@gmail.com>
2022-01-14 17:13:56 +0530

    from spring initializer

---
+++ restful-web-services/src/test/java/com/binaypaul/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaypaul.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
